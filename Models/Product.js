08a6091f339352cbf9e80557f695ee3a04b9b790
Mehedi Hasan <mehedi.hasan4846@gmail.com>
2021-11-11 04:27:18 +0600

    Created restfull api for user, products, cart, order

---
+++ Models/Product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    size: { type: Array },
+    color: { type: Array },
+    elevator: { type: Boolean, default: false },
+    innerSole: { type: Array },
+    outerHeel: { type: Array },
+    price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
---
+++ index.js
@@ -0,0 +1,34 @@
+// imports
+const authRoute = require("./Routes/authentication");
+
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config(); // environment variables
+const mongoose = require("mongoose");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.44dd5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const dbConnection = () => {
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB connected Successfully"))
+    .catch((err) => {
+      console.log("Error:" + err);
+    });
+};
+
+dbConnection();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan());
+
+// Routes
+app.use("/api/auth", authRoute);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
